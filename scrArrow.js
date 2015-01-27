5a99a09acd5a7f1b4617c9d4ede289d8e39663ea
kutiman <kutiman@gmail.com>
2015-01-27 16:27:54 +0530

    arrows

--- scrArrow.js
+++ scrArrow.js
@@ -1,23 +1,29 @@
 ﻿#pragma strict
 
 var direction = 1; // -1 if the arrow flies to the left
+var hit = false;
 
 function Start () {
 	if (direction == -1) {
 		transform.rotation.y = 180;
 	}
+	yield WaitForSeconds(3);
+	Destroy(gameObject);
 }
 
 function Update () {
-	FlyAcross();
+	if (!hit) {FlyAcross();}
 }
 
 function FlyAcross () {
-	var flySpeed = 12.0;
+	var flySpeed = 10.0;
 	transform.position.x += flySpeed * Time.deltaTime * direction;
 }
 
 function OnTriggerEnter (other : Collider) {
 	Debug.Log("Hit");
-	Destroy(other.gameObject);
+	if (!hit && other.gameObject.tag == "Player") {
+		hit = true;
+		transform.parent = other.gameObject.transform;
+	}
 }
--- scrPlayer.js
+++ scrPlayer.js
@@ -1,39 +1,51 @@
 ﻿#pragma strict
 
-var gravity = 0.1;
-var momentum = 0.0;
 var jumping = false;
- 
+var verticalVelocity = 0.0;
 
 function Start () {
 
 }
 
-function Update () {
-	Move();
-}
-
 function Jump () {
 	var jumpLength = 4;
 	jumping = true;
-	rigidbody.velocity.y += jumpLength;
+	verticalVelocity = jumpLength;
 }
 
 
 function Move () {
 	var walkSpeed = 2;
-	if (Input.GetKeyDown(KeyCode.UpArrow) && !jumping) {
-		Jump();
-	}
-	if (Input.GetKey(KeyCode.RightArrow) && !jumping) {
-		transform.position.x += walkSpeed * Time.deltaTime;
-	}
-	else if (Input.GetKey(KeyCode.LeftArrow) && !jumping) {
-		transform.position.x -= walkSpeed * Time.deltaTime;
-	} 
+	if (jumping) {}
+	else if (Input.GetKeyDown(KeyCode.UpArrow)) {Jump();}
+	else if (Input.GetKey(KeyCode.RightArrow)) {transform.position.x += walkSpeed * Time.deltaTime;}
+	else if (Input.GetKey(KeyCode.LeftArrow)) {transform.position.x -= walkSpeed * Time.deltaTime;}
+	transform.position.y += verticalVelocity * Time.deltaTime;
 }
 
-function OnCollisionEnter () {
-	jumping = false;
+function StayInBorders () {
+	var edge = 4.0;
+	var ground = 0.0;
+	if (transform.position.x < -edge) {transform.position.x = -edge;}
+	if (transform.position.x > edge) {transform.position.x = edge;}
+	if (transform.position.y < ground) {
+		transform.position.y = ground; 
+		verticalVelocity = 0.0; 
+		jumping = false;
+	}
+	if (transform.position.y > edge) {transform.position.y = ground;}
 }
+
+function Gravity () {
+	var gravity = 12;
+	verticalVelocity -= gravity * Time.deltaTime;
+}
+
+function Update () {
+	Move();
+	StayInBorders();
+	Gravity();
+}
+
+
 
